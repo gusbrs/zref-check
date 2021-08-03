3ca514265f45b133d3e152e016652d84fa327adb
gusbrs <19410606+gusbrs@users.noreply.github.com>
2021-08-03 09:31:15 -0300

    Add copyright support to update_tag in build.lua

--- build.lua
+++ build.lua
@@ -12,28 +12,68 @@ typesetfiles = {"*.tex"}
 -- Two runs for label testing
 checkruns = 2
 
--- Set version and release date automatically
-tagfiles = {"zref-check.dtx", "CHANGELOG.md"}
+-- Set version, release date and copyright automatically
+tagfiles = {"zref-check.dtx",
+            "CHANGELOG.md",
+            "zref-check.ins",
+            "zref-check.tex",
+            "zref-check-code.tex"}
 
 function update_tag(file, content, tagname, tagdate)
    tagname = string.gsub(tagname, "^v", "")
+   local tagyear = string.match(tagdate, "%d%d%d%d")
    if string.match(file, "^zref%-check%.dtx$") then
       content = string.gsub(
          content,
          "\n\\ProvidesExplPackage %{zref%-check%} %{[^}]+%} %{[^}]+%}\n",
          "\n\\ProvidesExplPackage {zref-check} {"
          .. tagdate .. "} {" .. tagname .. "}\n")
+      if string.match(content, "Copyright%D-%d%d%d%d%-%d%d%d%d") then
+         if not string.find(
+            content, "Copyright%D-%d%d%d%d*%-" .. tagyear) then
+            content = string.gsub(
+               content,
+               "Copyright(%D-)(%d%d%d%d%-)%d%d%d%d",
+               "Copyright%1%2" .. tagyear)
+         end
+      else
+         if not string.find(content, "Copyright%D-" .. tagyear) then
+            content = string.gsub(
+               content,
+               "Copyright(%D-)(%d%d%d%d)",
+               "Copyright%1%2-" .. tagyear)
+         end
+      end
    elseif string.match(file, "^CHANGELOG%.md$") then
       local url = "https://github.com/gusbrs/zref-check/compare/"
       content = string.gsub(
          content,
          "(## %[Unreleased%]%(.-)%.%.%.HEAD%)",
-         "%1...v" .. tagname .. ")")
+         "%1...v" .. tagname .. "), " .. tagdate)
       content = string.gsub(
          content,
          "## %[Unreleased%]",
          "## [Unreleased](" .. url .. "v" .. tagname .. "...HEAD)\n\n"
-         .. "## [v" .. tagname .."]")
+         .. "## [v" .. tagname .. "]")
+   elseif string.match(file, "^zref%-check%.ins$") or
+      string.match(file, "^zref%-check%.tex$") or
+      string.match(file, "^zref%-check%-code%.tex$") then
+      if string.match(content, "Copyright%D-%d%d%d%d%-%d%d%d%d") then
+         if not string.find(
+            content, "Copyright%D-%d%d%d%d*%-" .. tagyear) then
+            content = string.gsub(
+               content,
+               "Copyright(%D-)(%d%d%d%d%-)%d%d%d%d",
+               "Copyright%1%2" .. tagyear)
+         end
+      else
+         if not string.find(content, "Copyright%D-" .. tagyear) then
+            content = string.gsub(
+               content,
+               "Copyright(%D-)(%d%d%d%d)",
+               "Copyright%1%2-" .. tagyear)
+         end
+      end
    end
    return content
 end
