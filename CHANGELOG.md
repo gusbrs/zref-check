347061844fe5f1ac3add811130ad0d96fc43e122
gusbrs <19410606+gusbrs@users.noreply.github.com>
2021-08-02 21:12:51 -0300

    Set update_tag function in build.lua

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,6 @@
 # Changelog
 
-## [Unreleased]
-
-## [v0.1.0]
+## [Unreleased](https://github.com/gusbrs/zref-check/compare/v0.1.0...HEAD)
 
 ### Added
 - Initial release.
--- build.lua
+++ build.lua
@@ -11,3 +11,29 @@ typesetfiles = {"*.tex"}
 
 -- Two runs for label testing
 checkruns = 2
+
+-- Set version and release date automatically
+tagfiles = {"zref-check.dtx", "CHANGELOG.md"}
+
+function update_tag(file, content, tagname, tagdate)
+   tagname = string.gsub(tagname, "^v", "")
+   if string.match(file, "^zref%-check%.dtx$") then
+      content = string.gsub(
+         content,
+         "\n\\ProvidesExplPackage %{zref%-check%} %{[^}]+%} %{[^}]+%}\n",
+         "\n\\ProvidesExplPackage {zref-check} {"
+         .. tagdate .. "} {" .. tagname .. "}\n")
+   elseif string.match(file, "^CHANGELOG%.md$") then
+      local url = "https://github.com/gusbrs/zref-check/compare/"
+      content = string.gsub(
+         content,
+         "(## %[Unreleased%]%(.-)%.%.%.HEAD%)",
+         "%1...v" .. tagname .. ")")
+      content = string.gsub(
+         content,
+         "## %[Unreleased%]",
+         "## [Unreleased](" .. url .. "v" .. tagname .. "...HEAD)\n\n"
+         .. "## [v" .. tagname .."]")
+   end
+   return content
+end
